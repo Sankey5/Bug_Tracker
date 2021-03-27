f4aaa96c965a66c43d3c66718017d5da4c4ea9c4
Sankey5 <briansankey93@gmail.com>
2021-03-27 18:09:42 -0500

    Changed app run context

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from endpoints import bugCRUD   # Import endpoints
 from gateway import gateway     # Import gateway startup
+import os
 
 # Create the flask application and make it CORS compliant
 app = Flask(__name__)
@@ -19,4 +20,6 @@ app.add_url_rule('/bugs', view_func=bugCRUD.createBug, methods=['POST'])
 
 # Run it
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context=(os.path.abspath('resources/zbe976_cert.pem'),
+                         os.path.abspath('resources/zbe976_key.pem'))
+            , port=8080)
